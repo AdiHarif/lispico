685db9d0a4073ddf90f52ca75ac5a000c649e9d1
Adi Harif <adi.harif@campus.technion.ac.il>
2024-12-10 13:50:33 +0200

    Adds Sexp enum constructed out of pest parser output

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,36 @@
-use pest::Parser;
+use pest::{iterators::Pair, Parser};
 use pest_derive::Parser;
 
 #[derive(Parser)]
 #[grammar = "mylisp.pest"]
 struct MylispParser;
+
+#[derive(Debug)]
+enum Atom {
+    Nil,
+    Identifier(String),
+}
+
+#[derive(Debug)]
+enum Sexp {
+    Atom(Atom),
+    Cons(Box<Sexp>, Box<Sexp>),
+}
+
+fn construct_sexp(pair: Pair<Rule>) -> Sexp {
+    match pair.as_rule() {
+        Rule::nil => Sexp::Atom(Atom::Nil),
+        Rule::identifier => Sexp::Atom(Atom::Identifier(pair.as_str().to_string())),
+        Rule::seq => {
+            let mut seq = pair.into_inner();
+            Sexp::Cons(
+                Box::new(construct_sexp(seq.next().unwrap())),
+                Box::new(construct_sexp(seq.next().unwrap())),
+            )
+        }
+        _ => unreachable!("unexpected rule: {:?}", pair.as_rule()),
+    }
+}
 
 fn main() {}
 

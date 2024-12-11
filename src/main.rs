95c96015944cce1fb5da77fe9878ead38fed67a3
Adi Harif <adi.harif@campus.technion.ac.il>
2024-12-11 20:38:55 +0200

    Adds Sexp construction tests to main file

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ use pest_derive::Parser;
 #[grammar = "mylisp.pest"]
 struct MylispParser;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Atom {
     Nil,
     Identifier(String),
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Sexp {
     Atom(Atom),
     Cons(Box<Sexp>, Box<Sexp>),
@@ -57,6 +57,98 @@ mod tests {
         let faulty_programs = vec!["(", ")", "(a", "a)", "(a b", "(a b c"];
         for program in faulty_programs {
             assert!(MylispParser::parse(Rule::program, program).is_err());
+        }
+    }
+
+    #[test]
+    fn test_sexp() {
+        let programs = vec![
+            (
+                "(a)",
+                Sexp::Cons(
+                    Box::new(Sexp::Atom(Atom::Identifier("a".to_string()))),
+                    Box::new(Sexp::Atom(Atom::Nil)),
+                ),
+            ),
+            (
+                "(a b)",
+                Sexp::Cons(
+                    Box::new(Sexp::Atom(Atom::Identifier("a".to_string()))),
+                    Box::new(Sexp::Cons(
+                        Box::new(Sexp::Atom(Atom::Identifier("b".to_string()))),
+                        Box::new(Sexp::Atom(Atom::Nil)),
+                    )),
+                ),
+            ),
+            (
+                "(a b c)",
+                Sexp::Cons(
+                    Box::new(Sexp::Atom(Atom::Identifier("a".to_string()))),
+                    Box::new(Sexp::Cons(
+                        Box::new(Sexp::Atom(Atom::Identifier("b".to_string()))),
+                        Box::new(Sexp::Cons(
+                            Box::new(Sexp::Atom(Atom::Identifier("c".to_string()))),
+                            Box::new(Sexp::Atom(Atom::Nil)),
+                        )),
+                    )),
+                ),
+            ),
+            ("()", Sexp::Atom(Atom::Nil)),
+            (
+                "(())",
+                Sexp::Cons(
+                    Box::new(Sexp::Atom(Atom::Nil)),
+                    Box::new(Sexp::Atom(Atom::Nil)),
+                ),
+            ),
+            (
+                "((a))",
+                Sexp::Cons(
+                    Box::new(Sexp::Cons(
+                        Box::new(Sexp::Atom(Atom::Identifier("a".to_string()))),
+                        Box::new(Sexp::Atom(Atom::Nil)),
+                    )),
+                    Box::new(Sexp::Atom(Atom::Nil)),
+                ),
+            ),
+            (
+                "(a (b))",
+                Sexp::Cons(
+                    Box::new(Sexp::Atom(Atom::Identifier("a".to_string()))),
+                    Box::new(Sexp::Cons(
+                        Box::new(Sexp::Cons(
+                            Box::new(Sexp::Atom(Atom::Identifier("b".to_string()))),
+                            Box::new(Sexp::Atom(Atom::Nil)),
+                        )),
+                        Box::new(Sexp::Atom(Atom::Nil)),
+                    )),
+                ),
+            ),
+            (
+                "(a (b c))",
+                Sexp::Cons(
+                    Box::new(Sexp::Atom(Atom::Identifier("a".to_string()))),
+                    Box::new(Sexp::Cons(
+                        Box::new(Sexp::Cons(
+                            Box::new(Sexp::Atom(Atom::Identifier("b".to_string()))),
+                            Box::new(Sexp::Cons(
+                                Box::new(Sexp::Atom(Atom::Identifier("c".to_string()))),
+                                Box::new(Sexp::Atom(Atom::Nil)),
+                            )),
+                        )),
+                        Box::new(Sexp::Atom(Atom::Nil)),
+                    )),
+                ),
+            ),
+        ];
+
+        for (program, expected) in programs {
+            let pairs = MylispParser::parse(Rule::list, program)
+                .unwrap()
+                .next()
+                .unwrap();
+            let sexp = construct_sexp(pairs);
+            assert_eq!(sexp, expected, "program: {}", program);
         }
     }
 }

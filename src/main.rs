6b07fb96531819b868b95452e1b4a426486a7096
Adi Harif <adi.harif@campus.technion.ac.il>
2024-12-12 21:05:53 +0200

    Adds evaluating Sexp

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,54 @@ use pest_derive::Parser;
 #[grammar = "mylisp.pest"]
 struct MylispParser;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum Atom {
     Nil,
     Identifier(String),
 }
 
-#[derive(Debug, PartialEq)]
+impl Atom {
+    fn eval(&self) -> Sexp {
+        match self {
+            Atom::Nil => Sexp::Atom(Atom::Nil),
+            Atom::Identifier(_) => Sexp::Atom(Atom::Nil),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Clone)]
 enum Sexp {
     Atom(Atom),
     Cons(Box<Sexp>, Box<Sexp>),
+}
+
+impl Sexp {
+    fn car(&self) -> &Sexp {
+        match self {
+            Sexp::Cons(car, _) => car,
+            _ => panic!("Expected a cons, but got an atom"),
+        }
+    }
+
+    fn cdr(&self) -> &Sexp {
+        match self {
+            Sexp::Cons(_, cdr) => cdr,
+            _ => panic!("Expected a cons, but got an atom"),
+        }
+    }
+
+    fn eval(self) -> Sexp {
+        match self {
+            Sexp::Atom(atom) => atom.eval(),
+            Sexp::Cons(car, cdr) => {
+                if let Sexp::Atom(Atom::Identifier(identifier)) = *car {
+                    return evalFunction(Sexp::Atom(Atom::Identifier(identifier)), *cdr);
+                } else {
+                    panic!("Expected an identifier, but got an atom");
+                }
+            }
+        }
+    }
 }
 
 fn construct_sexp(pair: Pair<Rule>) -> Sexp {
@@ -36,6 +74,23 @@ fn construct_sexp(pair: Pair<Rule>) -> Sexp {
             )),
         ),
         _ => unreachable!("unexpected rule: {:?}", pair.as_rule()),
+    }
+}
+
+fn evalFunction(operator: Sexp, args: Sexp) -> Sexp {
+    match operator {
+        Sexp::Atom(Atom::Identifier(identifier)) => match identifier.as_str() {
+            "." => Sexp::Cons(
+                Box::new(args.car().clone().eval()),
+                Box::new(args.cdr().car().clone().eval()),
+            ),
+            ".<" => args.car().clone().eval().car().clone(),
+            ".>" => args.car().clone().eval().cdr().clone(),
+            "'" => args.car().clone(),
+            _ => panic!("Unknown operator: {}", identifier),
+        },
+        Sexp::Atom(_) => panic!("Expected an identifier, but got an atom"),
+        Sexp::Cons(_, _) => panic!("Expected an identifier, but got a list"),
     }
 }
 
@@ -243,6 +298,37 @@ mod tests {
         for identifier in invalid_identifiers {
             let pairs = MylispParser::parse(Rule::identifier, identifier);
             assert!(pairs.is_err(), "identifier: {}", identifier);
+        }
+    }
+
+    #[test]
+    fn test_eval() {
+        let programs = vec![
+            ("()", Sexp::Atom(Atom::Nil)),
+            (
+                "(. 'a 'b)",
+                Sexp::Cons(
+                    Box::new(Sexp::Atom(Atom::Identifier("a".to_string()))),
+                    Box::new(Sexp::Atom(Atom::Identifier("b".to_string()))),
+                ),
+            ),
+            ("(.< '(a b))", Sexp::Atom(Atom::Identifier("a".to_string()))),
+            (
+                "(.> '(a b))",
+                Sexp::Cons(
+                    Box::new(Sexp::Atom(Atom::Identifier("b".to_string()))),
+                    Box::new(Sexp::Atom(Atom::Nil)),
+                ),
+            ),
+        ];
+
+        for (program, expected) in programs {
+            let pairs = MylispParser::parse(Rule::list, program)
+                .unwrap()
+                .next()
+                .unwrap();
+            let sexp = construct_sexp(pairs);
+            assert_eq!(sexp.eval(), expected, "program: {}", program);
         }
     }
 }

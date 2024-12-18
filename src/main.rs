1196acc19e96113c145002d597837a58de6d545a
Adi Harif <adi.harif@campus.technion.ac.il>
2024-12-18 23:13:10 +0200

    Implements a simple repl

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
+use std::io::{self, Write};
+
+use parser::LispicoParser;
+use pest::Parser;
+
 mod exp;
 mod parser;
 mod tests;
 
-fn main() {}
+fn main() -> io::Result<()> {
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        if io::stdin().read_line(&mut input)? == 0 {
+            return Ok(());
+        }
+
+        let pair = LispicoParser::parse(parser::Rule::program, &input)
+            .expect("failed to parse input")
+            .next()
+            .unwrap();
+
+        let exp = parser::construct_exp(pair);
+        println!("{}", exp.eval());
+    }
+}
